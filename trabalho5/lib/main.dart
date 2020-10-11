4eb7cd524201ecf8ab218734ba0c59b85d8a024f
Lucas <44686243+lucaslinhares@users.noreply.github.com>
2020-10-10 22:06:56 -0300

    Add files via upload

---
+++ trabalho5/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:trabalho5/view/lista_curso.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(CursoApp());
+}
+
+class CursoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.orange[800],
+        accentColor: Colors.orange[800],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.orange[800],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaCurso(),
+    );
+  }
+}
